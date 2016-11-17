039c9c75b5b936bef08b72c0cb37abc675c1f971
AlexWang <1669499355@qq.com>
2016-11-17 15:14:03 +0800

    Commit Initial - UMD

---
+++ webpack.config.umd.js
@@ -0,0 +1,14 @@
+module.exports = function (umdConf) {
+  umdConf.devServer.host = '0.0.0.0';
+  umdConf.webpackFeatures.enableEntryHTML();
+
+  if (umdConf.devMode) {
+    umdConf.webpackFeatures.enableEntryHot();
+    umdConf.output.publicPath = '';
+  } else {
+    umdConf.webpackFeatures.enableUglifyJs({
+      comments: false
+    });
+  }
+};
+
