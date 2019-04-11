ee4799dd5b8d2a6ddf1df09beefe6c8d17514747
steven <wangxinwang@100tal.com>
2019-04-11 09:25:13 +0800

    onoff support

--- src/main.js
+++ src/main.js
@@ -8,84 +8,97 @@
 //  Author: AlexWang
 //  Date: 2016-09-03 23:58:56
 //  QQ Email: 1669499355@qq.com
-//  Last Modified time: 2018-02-02 18:47:35 by {{last_modified_by}}
+//  Last Modified time: 2018-02-03 16:13:52 by {{last_modified_by}}
 //  Description: tinyEmiter
 //
 // //////////////////////////////////////////////////////////////////////////////
 
 export default class Emiter {
   constructor() {
-    this.MAPS = {};
+    this.MAPS = {}
   }
 
   on(eid, elistener, context) {
-    if (context) { elistener.__this = context }
-    if (eid && elistener) {
-      const stub = this.MAPS[eid];
-      if (!stub) {
-        this.MAPS[eid] = [elistener];
-        return
-      }
+    if (!(eid && elistener)) throw new Error('eid, elistener are required!')
 
-      stub.push(elistener);
+    if (context) {
+      elistener.__this = context
     }
-  }
 
-  once(eid, elistener, context) {
-    if (context) { elistener.__this = context }
-    elistener.__once = true;
-    this.on(eid, elistener);
-  }
+    let stub = this.MAPS[eid]
 
-  has(eid) {
-    if (eid) {
-      const stub = this.MAPS[eid];
-      return stub && stub.length;
+    if (stub) {
+      stub.push(elistener)
+    } else {
+      stub = [elistener]
+      this.MAPS[eid] = stub
     }
-    return 0;
+
+    if (Object.entries(this.MAPS[eid]).length === 1 && this.onoff) this.onoff(true)
   }
 
   off(eid, elistener) {
     if (eid) {
-      const stub = this.MAPS[eid];
+      const stub = this.MAPS[eid]
       if (stub) {
         if (elistener) {
-          return stub.splice(stub.indexOf(elistener), 1);
+          stub.splice(stub.indexOf(elistener), 1)
+        } else {
+          console.warn(`**EVENT CODE ID is ${eid}, its all listeners will be removed.**`)
+          stub.length = 0
         }
-        console.warn(`**EVENT CODE ID is ${eid}, its all listeners will be removed.**`)
-        stub.length = 0;
+
+        if (stub.length === 0) delete this.MAPS[eid]
       }
     } else {
-      this.MAPS = {};
       console.warn('**EVENT CODE ID is empty, all listeners will be removed.**')
+      this.MAPS = {}
     }
+
+    if (Object.entries(this.MAPS).length === 0 && this.onoff) this.onoff()
+  }
+
+  has(eid) {
+    if (eid) {
+      const stub = this.MAPS[eid]
+      return stub ? stub.length : 0
+    }
+    return 0
+  }
+
+  once(eid, elistener, context) {
+    if (context) {
+      elistener.__this = context
+    }
+    elistener.__once = true
+    this.on(eid, elistener)
   }
 
   emit(eid, data) {
     if (eid) {
-      const onceElisteners = [];
+      const onceElisteners = []
       // eid=* broadcast
-      const asteriskStub = this.MAPS['*'];
+      const asteriskStub = this.MAPS['*']
       if (asteriskStub && asteriskStub.length) {
-        asteriskStub.forEach((elistener) => {
-          elistener.call(elistener.__this, eid, data);
-          if (elistener.__once) onceElisteners.push(elistener);
+        asteriskStub.forEach(elistener => {
+          elistener.call(elistener.__this, eid, data)
+          if (elistener.__once) onceElisteners.push(elistener)
         })
       }
 
       // eid= normal
-      const stub = this.MAPS[eid];
+      const stub = this.MAPS[eid]
       if (stub && stub.length) {
-        stub.forEach((elistener) => {
-          elistener.call(elistener.__this, data);
-          if (elistener.__once) onceElisteners.push(elistener);
-        });
+        stub.forEach(elistener => {
+          elistener.call(elistener.__this, data)
+          if (elistener.__once) onceElisteners.push(elistener)
+        })
       }
 
       // once
       if (onceElisteners.length) {
-        onceElisteners.forEach((elistener) => {
-          this.off(eid, elistener);
+        onceElisteners.forEach(elistener => {
+          this.off(eid, elistener)
         })
       }
     }
--- test/index.js
+++ test/index.js
@@ -1,2 +1,15 @@
-console.log('test cases have not been finished.');
+import E from '../src/main.js';
 
+const e = new E
+const f = function ecb() {
+  console.log(this)
+}
+const thisobj = { a: 'av' }
+
+const bindf = f.bind(thisobj)
+
+e.once('CODE_ID', bindf)
+
+setInterval(() => {
+  e.emit('CODE_ID', Math.random() * 1000)
+}, 1000)
